bcef605b63bb116e2a43591d776c936be2aa16b4
Paul <paulevers1311@gmail.com>
2021-04-04 16:18:20 +0200

    Map settings

--- spots/lib/screens/map.dart
+++ spots/lib/screens/map.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class MapScreen extends StatefulWidget {
   @override
@@ -10,6 +11,7 @@ class MapScreen extends StatefulWidget {
 
 class MapScreenState extends State<MapScreen> {
   Completer<GoogleMapController> _controller = Completer();
+  String _mapStyle;
 
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
@@ -23,13 +25,25 @@ class MapScreenState extends State<MapScreen> {
       zoom: 19.151926040649414);
 
   @override
+  void initState() {
+    super.initState();
+    rootBundle.loadString('assets/map_style.txt').then((string) {
+      _mapStyle = string;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
+        mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
+        myLocationEnabled: true,
+        compassEnabled: false,
+        zoomControlsEnabled: false,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
+          controller.setMapStyle(_mapStyle);
         },
       ),
       floatingActionButton: FloatingActionButton.extended(
